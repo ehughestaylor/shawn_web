3afeb51a0eb112a0b22d21321d727ccb6eb390a6
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-02-25 20:41:00 -0500

    changed nav completely

--- js/script.js
+++ js/script.js
@@ -2,28 +2,29 @@
 
 $(document).ready(function(){
 	scrollConverter.activate();
+	
 	$('#contact').hide();
 	$('#social').hide();
 	$('#pricing').hide();
 	$('#header').fadeIn();
 
-$('.contact').on('click',function(){
-	$(this).toggleClass('active');
-	$('#contact').toggle("slide", { direction: "right"  }, 500);
-});
+	$('.contact').on('click',function(){
+		$(this).toggleClass('active');
+		$('#contact').toggle("slide", { direction: "right"  }, 500);
+	});
 
-$('.social').on('click',function(){
-	$(this).toggleClass('active');
-	$('#social').toggle("slide", { direction: "left"  }, 500);
-});
+	$('.social').on('click',function(){
+		$(this).toggleClass('active');
+		$('#social').toggle("slide", { direction: "left"  }, 500);
+	});
 
-$('.pricing').on('click',function(){
-	$(this).toggleClass('active');
-    $('#pricing').toggle("slide", { direction: "up"  }, 500);
-});
+	$('.pricing').on('click',function(){
+		$(this).toggleClass('active');
+	    $('#pricing').toggle("slide", { direction: "up"  }, 500);
+	});
 
-$('#my_elem').mousewheel(function(event) {
-    console.log(event.deltaX, event.deltaY, event.deltaFactor);
-});
+	$('#my_elem').mousewheel(function(event) {
+	    console.log(event.deltaX, event.deltaY, event.deltaFactor);
+	});
 
 });
